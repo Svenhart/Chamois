4055a9c06acc39b46a84f85699558ce536ffe8ac
Svenhart <andrei-nicolas@outlook.fr>
2019-11-28 20:15:36 +0100

    Clean commit ??

---
+++ Assets/Scripts/Capture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capture : MonoBehaviour
+{
+    private Patrol preyPatrol;
+    
+    public Animator PopUpAnimator;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.GetComponent<Patrol>()!= null )
+        {
+            this.preyPatrol = other.GetComponent<Patrol>();
+            this.preyPatrol.Respawn();
+            this.preyPatrol = null;
+            PopUpAnimator.SetTrigger("Capture");
+        }
+    }
+}
